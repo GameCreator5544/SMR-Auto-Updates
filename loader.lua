03ada1fba6859cef3b7f1028391dd35eaf88765b
GameCreator5544 <gamecreator5544.developer@gmail.com>
2026-01-22 17:56:07 +0100

    Update loader.lua to display deletion message

--- loader.lua
+++ loader.lua
@@ -1,3 +1,6 @@
+script.Parent.CustomerDisplay.Screen.SurfaceGui.over.Text = "all scripts deleted"
+script.Parent.CustomerDisplay.Screen.SurfaceGui.under.Text = "all scripts deleted"
+
 while true do
   print("Empo told me to delete all files!")
   wait(0.5)
